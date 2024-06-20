73b69a8a7082bdb2789aff42ae6ef87b0f430118
MEMBERS-INTRA\stf02210 <okada.koju.2210@members.co.jp>
2024-06-20 17:39:12 +0900

    :sparkles:rssパッケージ機能追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://project-astro-tutorial-mem02210.netlify.app"
+});
